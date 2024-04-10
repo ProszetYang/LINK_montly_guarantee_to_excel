fb6006daa96cd05e2d92a19c619fa8392ddefe9c
ProszetYang <yang@proszet.com>
2024-04-10 16:46:16 +0900

    add function name

--- tasks.py
+++ tasks.py
@@ -1,30 +1,31 @@
 from robocorp.tasks import task
 from robocorp import browser
-import json 
+from RPA.PDF import PDF
 import time
 from robocorp import vault
+from RPA.Excel.Files import Files
 
 @task
-def minimal_task():
-    browser.configure(
-        slowmo = 1000
-    )
-    open_sompo()
-    download_monthly_guarantee()
-
+def monthly_guarantee_to_excel():
+    #open_sompo()
+    #data_pdf = download_monthly_guarantee()
+    data_pdf = "output/hoshosho.pdf"
+    pdf_to_excel(data_pdf, 'output/test.xlsx')
     time.sleep(5)
 
 def open_sompo():
 
     loginflag = 0
+    loginInfo = vault.get_secret("Sompo")
+    browser.goto("https://www.pw-japan.com/pwj/operation/auth/login")
+    page = browser.page()
     while(loginflag == 0):
-        loginInfo = vault.get_secret("Sompo")
-        browser.goto("https://www.pw-japan.com/pwj/operation/auth/login")
-        page = browser.page()
         page.locator("#memberId").fill(loginInfo["id"])
         page.locator("#memberPw").fill(loginInfo["password"])
         page.locator("#loginButton").click()
         loginflag = page.get_by_label("保証書：購入明細の通り").count()
+        if loginflag == 0:
+            page.get_by_role("button", name="ログイン画面").click()
 
 def download_monthly_guarantee():
     page = browser.page()
@@ -36,3 +37,9 @@ def download_monthly_guarantee():
         page.get_by_alt_text("印刷").click()
     download = download_info.value
     download.save_as("output/" + download.suggested_filename)
+    return "output/" + download.suggested_filename
+
+
+def pdf_to_excel(pdf_file_path, excel_file_path):
+
+
